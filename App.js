0e66793834f5d0236ffffe849c21a8b804504640
benkassabian <30545571+benkassabian@users.noreply.github.com>
2022-11-16 13:44:03 +1100

    quiz and calculator
    did the quiz and the calculator

--- App.js
+++ App.js
@@ -7,15 +7,21 @@ import theme from "./src/styles/theme";
 import background from "./assets/background.png";
 import LandingPage from "./src/pages/LandingPage";
 import SignUpPage from "./src/pages/SignUpPage";
+import JumpingGame from "./src/pages/JumpingGame";
 import LoginPage from "./src/pages/LoginPage";
 import NavBar from "./src/components/NavBar";
+import Bird from "./src/components/Bird";
+import Quiz from "./src/pages/Quiz";
+import Calculator from "./src/pages/Calculator";
+
+
 
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <ImageBackground style={styles.container} source={background}>
         {/* <LandingPage /> */}
-        <SignUpPage />
+        <Calculator />
         {/* <LoginPage /> */}
         {/* <NavBar /> */}
       </ImageBackground>
---
+++ src/components/QuizData.js
@@ -0,0 +1,27 @@
+export default data = [
+    {
+        question: "What is the best way to save water?",
+        options: ["Shower Less","Reduce Gardening","Wash Hands Quicker","Solar Power"],
+        correct_option: "Shower Less"
+    },
+    {
+        question: "How long should the average shower be?",
+        options: ["Chand Minar","Taj Mahal","Stadium"],
+        correct_option: "Taj Mahal"
+    },
+    {
+        question: "What land animal can open its mouth the widest?",
+        options: ["Alligator","Crocodile","Baboon","Hippo"],
+        correct_option: "Hippo"
+    },
+    {
+        question: "How long should the average shower be?",
+        options: ["1 Minute","5 Minutes","10 Minutes","30 Minutes"],
+        correct_option: "5 Minutes"
+    },
+    {
+        question: "What is the only flying mammal?",
+        options: ["The bat","The flying squirrel","The bald eagle","The colugo"],
+        correct_option: "The bat"
+    }
+]
